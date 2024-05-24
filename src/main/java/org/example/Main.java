b063588e05500fc7b056427378d6e4e1824cc553
fevzi-kayaonu <fevzikayaonu35@gmail.com>
2024-05-24 15:28:44 +0300

    testler tamamlandı

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import org.example.enums.Plan;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        mainTest();
+    }
+
+    public static void mainTest(){
+        String[] healtnames = new String[2];
+        String[] developnames = new String[6];
+
+        Employee employee = new Employee(120,"fevzi kayaönü","fevzi123","asd@asdasd.com",healtnames);
+        Company company = new Company(124,"fevzi",5.2,developnames);
+        HealthPlan healthplan = new HealthPlan(125,"fevzi", Plan.BASIC );
+
+        System.out.println(employee.toString());
+        System.out.println(company.toString());
+        System.out.println(healthplan.toString());
     }
 }
